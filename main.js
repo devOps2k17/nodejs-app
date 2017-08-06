a3c589e2e09955eff3ed50212a61d6620486235e
manee2k6 <manee2k6@gmail.com>
2017-08-05 20:54:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Aditya sinha\n");
+  response.end("Hello DevOps Students-Welcome..\n");
 });
 
 // listen on localhost:8000
